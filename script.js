bbdc970a702b4b3fe76fc0ac2f5b43198d369e2c
Willian Gonçalves <willian.p.g@outlook.com>
2023-05-17 16:59:10 -0300

    update do layout

--- script.js
+++ script.js
@@ -41,10 +41,10 @@ ScrollReveal(),reveal('.home-img, .services-container, portfolio-box, contact-fo
 ScrollReveal(),reveal('.home-content h1, .about-img', {origin: 'left'});
 ScrollReveal(),reveal('.home-content p, .about-content', {origin: 'right'});
 
-const typed = new Typed('.multiple-text', {
-  strings: ['Front-End Developer', 'Back-End Developer', 'Full-Stack Developer'],
-  typeSpeed: 100,
-  backSpeed: 100,
-  backDelay: 1000,
-  loop: true,
-});
+// const typed = new Typed('.multiple-text', {
+//   strings: ['Front-End Developer', 'Back-End Developer', 'Full-Stack Developer'],
+//   typeSpeed: 100,
+//   backSpeed: 100,
+//   backDelay: 1000,
+//   loop: true,
+// });
